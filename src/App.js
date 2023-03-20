0a00b09ef1019483f18c27315f70b89f33ee46b2
FabianUlises <ulisesfab.car@outlook.com>
2023-03-20 02:38:01 -0700

    Wrote test to check response object name is right. Also added fetch to pass test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 
 function App() {
+  // State
+  const [gitHubName, setGitHubName] = useState('');
+  useEffect(() => {
+    fetch('http://api.github.com/users/FabianUlises')
+    .then(res => res.json())
+    .then(data => {
+      setGitHubName(data.name);
+      console.log(data.bio);
+    })
+    .catch(err => console.log('==== ERROR ====', err));
+  }, []);
   return (
     <div className="App">
       <h1>Github Profile Info:</h1>
+      <h2>{gitHubName}</h2>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,11 @@
-
-
-
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App';
+beforeEach(() => {
+    fetch.resetMocks();
+});
+test('receives GitHub name from GitHub REST API using jest fetch mock', async() => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'Fabian'}));
+    render(<App />);
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }));
+    expect(gitHubName).toHaveTextContent('Fabian');
+});
